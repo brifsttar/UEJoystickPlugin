b5414f92297cdc0dfe5f965b8a0f5d615fd45e70
Bertrand Richard <bertrand.richard@ifsttar.fr>
2020-12-05 16:40:57 +0100

    Fixed bad DLL loading path

--- Source/JoystickPlugin/Private/JoystickPlugin.cpp
+++ Source/JoystickPlugin/Private/JoystickPlugin.cpp
@@ -24,7 +24,7 @@ IMPLEMENT_MODULE(FJoystickPlugin, JoystickPlugin)
 void FJoystickPlugin::StartupModule()
 {
 	IJoystickPlugin::StartupModule();
-	FString sdlDir("Plugins/UEJoystickPlugin/Plugins/JoystickPlugin/Source/ThirdParty/SDL2-2.0.10/lib/x64/SDL2.dll");
+	FString sdlDir("Plugins/JoystickPlugin/Source/ThirdParty/SDL2-2.0.10/lib/x64/SDL2.dll");
 	FString DllRelativePath = FPaths::ProjectDir() + sdlDir;
 	FString DllPath = IFileManager::Get().ConvertToAbsolutePathForExternalAppForRead(*DllRelativePath);
 	void* DLLHandle = FPlatformProcess::GetDllHandle(*DllPath);
